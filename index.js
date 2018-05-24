87fafdb840efdf2d897e09cd3d1563b7251e64f3
mario <mario.floresg@sngular.team>
2018-05-24 18:03:29 -0500

    Primer commit

---
+++ index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by administrador on 24/05/18.
+ */
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+});
