94b6ba6a397ae9e9d39414a75b17f2fdddde2ba2
juca004 <juca210784>
2018-06-21 10:02:51 -0500

    inserte la tabla pero no aparece

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 /**
  * Created by administrador on 24/05/18.
  */
-document.addEventListener('DOMContentLoaded', function () {
+ var arreglo = [{nombre:"Delia", fecha:new Date(1984, 10, 11), terreno:467, precio:323.53},
+                {nombre:"Bety", fecha:new Date(2002, 12, 30), terreno:268, precio:252.56},
+                {nombre:"Ernesto", fecha:new Date(2013, 05, 10), terreno:838, precio:285.87},
+                {nombre:"Ana", fecha:new Date(1975, 03, 15), terreno:932, precio:380.47},
+                {nombre:"Carlos", fecha:new Date(1989, 09, 28), terreno:592, precio:239.28}]
+
+document.addEventListener('DOMContentLoaded', function (event){
     var elems = document.querySelectorAll('.sidenav');
     M.Sidenav.init(elems);
-    
+
     var tabsElems = document.querySelectorAll('.tabs');
     M.Tabs.init(tabsElems);
 
@@ -20,61 +26,14 @@ document.addEventListener('DOMContentLoaded', function () {
 
     var dropdownElems = document.querySelectorAll('.dropdown-trigger');
     var instances = M.Dropdown.init(dropdownElems);
-    
-});
-
-function sortTable(n) {
-	  var table, rows, switching,i, x, y, shouldSwitch, dir, switchcount = 0;
-	  table = document.getElementById("myTable");
-	  switching = true;
-	  
-	  dir = "asc";
-	  
-	  while (switching) {
-	  
-	    switching = false;
-	    rows = table.getElementsByTagName("TR");
-	    
-	    for (i = 1; i < (rows.length - 1); i++) {
-	      
-	      shouldSwitch = false;
-	      
-	      x = rows[i].getElementsByTagName("TD")[n];
-	      y = rows[i + 1].getElementsByTagName("TD")[n];
-	      
-	      if (dir == "asc") {
-	          if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-	          
-	            shouldSwitch= true;
-	            break;
-	          }
-	        } else if (dir == "desc") {
-	          if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-	           
-	            shouldSwitch = true;
-	            break;
-	          }
-	        }
-	      }
-	      if (shouldSwitch) {
-
-	        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-	        switching = true;
-	      
-	        switchcount ++;      
-	      } else {
-	      
-	        if (switchcount == 0 && dir == "asc") {
-	          dir = "desc";
-	          switching = true;
-	        }
-	      }
-	    }
-	  }
-
-document.getElementById("demo").innerHTML = new Date("jul 21 1984");
 
 
+  const var_tabla = document.getElementById('mitabla');
+  let var_c = "<thead><tr> <th>Nombre</th> <th>Fecha</th> <th>Terreno en M^2</th> <th>Precio</th> </tr></thead>" ;
 
+  for (var i = 0; i < arreglo.length; i++) {
+    var_c += "<tbody><tr><td>" + arreglo[i].nombre + "</td><td>" + arreglo[i].fecha + "</td><td>" + arreglo[i].terreno + "</td><td>" + arreglo[i].precio + "</td></tr></tbody>" ;
+  }
+  var_tabla.inneHTML = var_c;
 
-
+  });
